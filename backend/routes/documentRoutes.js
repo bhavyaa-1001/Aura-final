f803a5c566bb04efcea50aa5c72963e5a447a709
bhavyaa-1001 <bhavyyaa106@gmail.com>
2025-08-14 21:24:38 +0530

    e

--- backend/routes/documentRoutes.js
+++ backend/routes/documentRoutes.js
@@ -3,10 +3,8 @@ const { uploadDocument, getDocuments, getDocument, verifyDocument } = require('.
 const upload = require('../middleware/uploadMiddleware');
 
 const router = express.Router();
-
 router.post('/', upload.single('file'), uploadDocument);
 router.get('/', getDocuments);
 router.get('/:id', getDocument);
 router.put('/:id/verify', verifyDocument);
-
 module.exports = router;
